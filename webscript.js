9eec13505c1583c9eb32aa9cc1d6f37f4983dd55
CNoble369 <68541682+CNoble369@users.noreply.github.com>
2020-12-12 15:21:27 +0530

    Add files via upload

---
+++ webscript.js
@@ -0,0 +1,11 @@
+function revealMessage() {
+    document.getElementById("hidden Message").style.display = 'block';
+}
+
+function showMessage() {
+    document.getElementById("some Message").style.display = 'block';
+}
+
+function anotherMessage() {
+    document.getElementById("another Message").style.display = 'block';
+}
